5c10d30fbb6e7b08d3e21e0d92ce40a4dbe0e021
Binny V A <binnyva@gmail.com>
2020-09-21 02:00:40 +0530

    Few iframe2 fixes

--- templates/layout/page.php
+++ templates/layout/page.php
@@ -4,11 +4,10 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php echo $title . (isset($page_title) ? " : $page_title" : '') ?></title>
-<link href="<?php echo $config['site_home'] ?>css/style.css" rel="stylesheet" type="text/css" />
-<link href="<?php echo $config['site_home'] ?>images/silk_theme.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo $config['app_url'] ?>css/style.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo $config['app_url'] ?>images/silk_theme.css" rel="stylesheet" type="text/css" />
 <link href="<?php echo $config['common_library_url'] ?>bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="<?php echo $config['common_library_url'] ?>bower_components/bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
-<link href="<?php echo $config['site_home'] ?>js/library/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css" rel="stylesheet" type="text/css" />
 <link href="<?php echo $config['common_library_url'] ?>css/style.css" rel="stylesheet" type="text/css" />
 <?php echo $css_includes ?>
 </head>
@@ -16,7 +15,7 @@
 <div id="wrapper" class="container">
 <div id="header" class="navbar navbar-fixed-top navbar-inverse" role="navigation">
 <div id="nav" class="container">
-	<div class="navbar-brand"><a href="<?php echo $config['site_home'] ?>"><?php echo $config['site_title'] ?></a></div>
+	<div class="navbar-brand"><a href="<?php echo $config['app_url'] ?>"><?php echo $config['app_name'] ?></a></div>
 
 	<div class="collapse navbar-collapse">
 	<ul id="nav-links" class="nav navbar-nav pull-right">
@@ -32,15 +31,15 @@
 ?></div>
 <div id="success-message" <?php echo ($QUERY['success']) ? '':'style="display:none;"';?>><?php echo strip_tags(stripslashes($QUERY['success']))?></div>
 
-<?php include($GLOBALS['template']->template); ?>
+<?php include(iapp('template')->template); ?>
 
 </div>
 <div id="footer"></div>
 </div>
 
-<script src="<?php echo $config['site_home'] ?>bower_components/jquery/dist/jquery.min.js" type="text/javascript"></script>
-<script src="<?php echo $config['site_home'] ?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="<?php echo $config['site_home'] ?>js/application.js" type="text/javascript"></script>
+<script src="<?php echo $config['app_url'] ?>bower_components/jquery/dist/jquery.min.js" type="text/javascript"></script>
+<script src="<?php echo $config['app_url'] ?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?php echo $config['app_url'] ?>js/application.js" type="text/javascript"></script>
 
 <?php echo $js_includes; ?>
 </body>
