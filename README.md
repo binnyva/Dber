3a25e793b7a4beb8a446aaf5fdb71838da65c0bb
Binny V A <binnyva@gmail.com>
2026-06-01 16:38:02 +0100

    Archiving this repo

---
+++ README.md
@@ -0,0 +1,14 @@
+# DBer
+
+Tool to convert DB Design to SQL Queries. My own non standard format of DB Design. Sample....
+
+
+Table
+	field_one
+	field_two INT
+
+
+
+#### Note
+
+Readme created on June 1, 2026 for code archival purpose based on my memory long after the code was created.
---
+++ configuration.php
@@ -0,0 +1,11 @@
+<?php
+//Configuration file for iFrame
+return array(
+	'app_name'		=>	'Dber',
+	'db_host'		=>	'localhost',
+	'db_user'		=>	'root',
+	'db_password'	=>	'',
+	'db_database'	=>	'makeadiff_madapp',
+	'app_url'		=>  'http://localhost/tools/Dber/',
+	'common_library_url'	=> 'http://localhost/MAD/apps/common/',
+);
